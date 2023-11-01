2d821f46db5436f0158cc39bc17822ffe260ba2f
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-11-02 00:43:08 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,16 @@ function App() {
   return (
     <main className='h-screen flex w-screen justify-center items-center bg-cream'>
         <div className='flex w-2/5 h-3/5 shadow-xl rounded-xl'>
-            <img className='rounded-l-xl' src={img} alt="" />
-          <div className='px-8 py-6'>
-            <h3 className='font-mont text-sm opacity-80'>PERFUME</h3>
-            <h2 className='font-Frau text-3xl text-black'>Gabreille Essence Eau De Parfum</h2>
-            <p className='font-Frau text-sm opacity-80'>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
-            <div className='flex'>
-              <p>$149.99</p>
-              <p>$169.99</p>
+            <img className='rounded-l-xl' src={img} alt="product" />
+          <div className='px-8 py-6 bg-white rounded-r-xl grid gap-2'>
+            <h3 className='font-mont text-xs opacity-70 tracking-widee'>PERFUME</h3>
+            <h2 className='font-Frau text-4xl text-black font-extrabold'>Gabreille Essence Eau De Parfum</h2>
+            <p className='font-Frau opacity-70 text-base font-medium'>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+            <div className='flex justify-between items-center'>
+              <p className='font-Frau text-3xl text-dc font-bold'>$149.99</p>
+              <p className='font-mont opacity-50 line-through'>$169.99</p>
             </div>
-            <button className='bg-green'> <img src={icon} alt="icon" /> Add to Cart</button>
+            <button className='bg-dc flex justify-center items-center text-white  rounded-md hover:bg-green-900'> <img src={icon} alt="icon" /> Add to Cart</button>
           </div>
         </div>
     </main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,23 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    ".src/App.jsx",
   ],
   theme: {
     extend: {
       fontFamily: {
         mont: ['Montserrat'],
         Frau: ['Fraunces']
+      },
+      colors: {
+        'dc': "hsl(158, 36%, 37%)",
+        'cream': "hsl(30, 38%, 92%)",
+      },
+      letterSpacing: {
+        "widee": "0.4em"
+      },
+      screens: {
+        'xs': '300px'
       }
     },
   },
